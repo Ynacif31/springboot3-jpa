1fe661adfc674d9e657b17e662087cca26f42622
Ygor Nacif <ygor.nacif00@gmail.com>
2024-09-12 14:13:00 -0300

    User entity and resource

---
+++ src/main/java/com/ygornacif/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.ygornacif.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ygornacif.course.entities.Users;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<Users> findAll(){
+		Users u = new Users(1L, "Ygor", "ygor@gmail.com", "21980161321", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
