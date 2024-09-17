80cc9c0600d9a7a15d6928ab96525cb2312d9d76
Ygor Nacif <ygor.nacif00@gmail.com>
2024-09-17 12:09:07 -0300

    Product entity

---
+++ src/main/java/com/ygornacif/course/services/ProductService.java
@@ -0,0 +1,28 @@
+package com.ygornacif.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ygornacif.course.entities.Product;
+import com.ygornacif.course.repositories.ProductRepository;
+
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
