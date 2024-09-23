3839752868765cca3ef1691a62a2e184cb4bcee2
Ygor Nacif <ygor.nacif00@gmail.com>
2024-09-23 17:41:16 -0300

    exception findById

--- src/main/java/com/ygornacif/course/services/UserService.java
+++ src/main/java/com/ygornacif/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.ygornacif.course.entities.User;
 import com.ygornacif.course.repositories.UserRepository;
+import com.ygornacif.course.services.exceptions.ResourceNotFoundException;
 
 
 @Service
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
